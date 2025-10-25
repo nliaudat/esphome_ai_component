822e489b8518d296e6352dfe0089ecd189e60b2f
Nicolas Liaudat <nliaudat@users.noreply.github.com>
2025-10-25 19:45:56 +0200

    add new models

--- components/meter_reader_tflite/model_config.h
+++ components/meter_reader_tflite/model_config.h
@@ -73,6 +73,58 @@ static const std::unordered_map<std::string, ModelConfig> MODEL_CONFIGS = {
             .normalize = true,
             .invert = true
         }
+    },
+    {"digit_recognizer_v4_10cls_GRAY", 
+        ModelConfig{
+            .description = "digit_recognizer_v4_10cls_GRAY",
+            .tensor_arena_size = "130KB", //check_tflite_model.py reports : Total Arena Size: 101.21 KB (doubling the model size seems to be validated in my tests)
+            .output_processing = "softmax",
+            .scale_factor = 1.0f,
+            .input_type = "uint8",  
+            .input_channels = 1,
+            .input_order = "RGB",
+            .input_size = {32, 20}, 
+            .normalize = true 
+        }
+    },
+    {"digit_recognizer_v4_10cls_RGB", 
+        ModelConfig{
+            .description = "digit_recognizer_v4_10cls_RGB",
+            .tensor_arena_size = "170KB", //check_tflite_model.py reports : Total Arena Size: 129.56 KB
+            .output_processing = "softmax",
+            .scale_factor = 1.0f,
+            .input_type = "uint8",  
+            .input_channels = 3,
+            .input_order = "RGB",
+            .input_size = {32, 20}, 
+            .normalize = true 
+        }
+    },
+    {"digit_recognizer_v4_100cls_GRAY", 
+        ModelConfig{
+            .description = "digit_recognizer_v4_100cls_GRAY",
+            .tensor_arena_size = "150KB", //check_tflite_model.py reports : Total Arena Size: 107.45 KB
+            .output_processing = "softmax",
+            .scale_factor = 10.0f,
+            .input_type = "uint8",  
+            .input_channels = 1,
+            .input_order = "RGB",
+            .input_size = {32, 20}, 
+            .normalize = true 
+        }
+    },
+    {"digit_recognizer_v4_100cls_RGB", 
+        ModelConfig{
+            .description = "digit_recognizer_v4_100cls_RGB",
+            .tensor_arena_size = "190KB", //check_tflite_model.py reports : Total Arena Size: 136.50 KB 
+            .output_processing = "softmax",
+            .scale_factor = 10.0f,
+            .input_type = "uint8",  
+            .input_channels = 3,
+            .input_order = "RGB",
+            .input_size = {32, 20}, 
+            .normalize = true 
+        }
     }
 };
 
