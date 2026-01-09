201ff1ea15ae56ebbf7c2669758144e1dc87e824
Nicolas Liaudat <nliaudat@users.noreply.github.com>
2026-01-09 16:10:22 +0100

    small fixes

--- components/ssocr_reader/value_validator_coordinator.cpp
+++ components/ssocr_reader/value_validator_coordinator.cpp
@@ -1,11 +1,11 @@
 #include "value_validator_coordinator.h"
 
 namespace esphome {
-namespace meter_reader_tflite {
+namespace ssocr_reader {
 
 // Currently implementation is fully inline in header for simplicity as it's just a wrapper.
 // Kept this file if we need to expand later or to satisfy build systems expecting .cpp
 // But for now it can be empty or just include the header.
 
-}  // namespace meter_reader_tflite
+}  // namespace ssocr_reader
 }  // namespace esphome
