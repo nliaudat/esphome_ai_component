d60ae040e122da520020fef6e07aea201cb664e3
Nicolas Liaudat <nliaudat@users.noreply.github.com>
2026-01-14 21:09:04 +0100

    Delete detect_legacy.cpp

--- components/analog_reader/detect_legacy.cpp
+++
@@ -1,4 +0,0 @@
-// detect_legacy.cpp is now DEPRECATED.
-// All algorithms are consolidated in multi_algorithm.cpp.
-// This file is kept empty to avoid compile errors if referenced by build scripts,
-// but its code is moved.
